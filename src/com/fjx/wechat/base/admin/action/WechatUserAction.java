91f00eeb721eb68d42fc6a19af7de6937666829a
FengJianxin <xd-fjx@qq.com>
2015-03-14 23:00:57 +0800

    格式化代码

--- src/com/fjx/wechat/base/admin/action/WechatUserAction.java
+++ src/com/fjx/wechat/base/admin/action/WechatUserAction.java
@@ -1,4 +1,19 @@
+
 package com.fjx.wechat.base.admin.action;
+
+import com.fjx.common.action.BaseController;
+import com.fjx.common.framework.system.pagination.Pagination;
+import com.fjx.wechat.base.admin.entity.SysUserEntity;
+import com.fjx.wechat.base.admin.entity.WechatUserEntity;
+import com.fjx.wechat.base.admin.entity.WechatUserGroupEntity;
+import com.fjx.wechat.base.admin.service.WechatUserGroupService;
+import com.fjx.wechat.base.admin.service.WechatUserService;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 import java.util.List;
@@ -6,103 +21,89 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.fjx.wechat.base.admin.service.WechatUserGroupService;
-import com.fjx.wechat.base.admin.service.WechatUserService;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.fjx.common.action.BaseController;
-import com.fjx.common.action.MyExecuteCallback;
-import com.fjx.common.framework.system.pagination.Pagination;
-import com.fjx.wechat.base.admin.entity.SysUserEntity;
-import com.fjx.wechat.base.admin.entity.WechatUserEntity;
-import com.fjx.wechat.base.admin.entity.WechatUserGroupEntity;
-
-
 /**
  * 微信用户管理
+ * 
  * @author fengjx xd-fjx@qq.com
  * @date 2014年11月2日
  */
 @Controller
 @RequestMapping("/admin/user")
 public class WechatUserAction extends BaseController {
-	
-	@Autowired
-	private WechatUserService wechatUserService;
-	@Autowired
-	private WechatUserGroupService wechatUserGroupService;
-	
-	@RequestMapping(value="")
-	public String view(HttpServletRequest request){
-		return "wechat/admin/user/user";
-	}
-	
-	@RequestMapping(value="/groupList")
-	@ResponseBody
-	public List<WechatUserGroupEntity> groupList(HttpServletRequest request){
-		return wechatUserGroupService.findList();
-	}
-	
-	
-	@RequestMapping(value="/userList")
-	@ResponseBody
-	public Pagination<WechatUserEntity> userList(HttpServletRequest request,WechatUserEntity wechatUser, String group_id){
-		SysUserEntity sysUser = getLoginSysUser(request);
-		return wechatUserService.pageList(wechatUser,group_id, sysUser.getWechatPublicAccount());
-	}
 
+    @Autowired
+    private WechatUserService wechatUserService;
+    @Autowired
+    private WechatUserGroupService wechatUserGroupService;
 
-	/**
-	 * 保存粉丝分组
-	 * @param request
-	 * @param userGroup
-	 * @return
-	 */
-	@RequestMapping(value="/saveGroup")
-	@ResponseBody
-	public Map<String, String> saveGroup(HttpServletRequest request, WechatUserGroupEntity userGroup){
-		setErrorMsg(request,"操作失败");
-		userGroup.setIn_time(new Date());
-		if(StringUtils.isBlank(userGroup.getId())){
-			wechatUserGroupService.save(userGroup);
-		}else{
-			wechatUserGroupService.update(userGroup);
-		}
-		return retSuccess();
-	}
+    @RequestMapping(value = "")
+    public String view(HttpServletRequest request) {
+        return "wechat/admin/user/user";
+    }
 
-	/**
-	 * 删除粉丝分组
-	 * @param request
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value="/deleteGroup")
-	@ResponseBody
-	public Map<String, String> deleteGroup(HttpServletRequest request,String id){
-		setErrorMsg(request,"操作失败");
-		wechatUserGroupService.delete(id);
-		return retSuccess();
-	}
+    @RequestMapping(value = "/groupList")
+    @ResponseBody
+    public List<WechatUserGroupEntity> groupList(HttpServletRequest request) {
+        return wechatUserGroupService.findList();
+    }
 
-	/**
-	 * 更新粉丝分组
-	 * @param request
-	 * @param user_id
-	 * @param group_id
-	 * @return
-	 */
-	@RequestMapping(value="/updateUser")
-	@ResponseBody
-	public Map<String, String> saveUser(HttpServletRequest request,String user_id, String group_id){
-		setErrorMsg(request,"操作失败");
-		wechatUserService.updateGroup(user_id, group_id);
-		return retSuccess();
-	}
-	
+    @RequestMapping(value = "/userPageList")
+    @ResponseBody
+    public Pagination<WechatUserEntity> userList(HttpServletRequest request,
+            WechatUserEntity wechatUser, String group_id) {
+        SysUserEntity sysUser = getLoginSysUser(request);
+        return wechatUserService.pageList(wechatUser, group_id, sysUser.getWechatPublicAccount());
+    }
+
+    /**
+     * 保存粉丝分组
+     * 
+     * @param request
+     * @param userGroup
+     * @return
+     */
+    @RequestMapping(value = "/saveGroup")
+    @ResponseBody
+    public Map<String, String> saveGroup(HttpServletRequest request, WechatUserGroupEntity userGroup) {
+        setErrorMsg(request, "操作失败");
+        userGroup.setIn_time(new Date());
+        if (StringUtils.isBlank(userGroup.getId())) {
+            wechatUserGroupService.save(userGroup);
+        } else {
+            wechatUserGroupService.update(userGroup);
+        }
+        return retSuccess();
+    }
+
+    /**
+     * 删除粉丝分组
+     * 
+     * @param request
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/deleteGroup")
+    @ResponseBody
+    public Map<String, String> deleteGroup(HttpServletRequest request, String id) {
+        setErrorMsg(request, "操作失败");
+        wechatUserGroupService.delete(id);
+        return retSuccess();
+    }
+
+    /**
+     * 更新粉丝分组
+     * 
+     * @param request
+     * @param user_id
+     * @param group_id
+     * @return
+     */
+    @RequestMapping(value = "/updateUser")
+    @ResponseBody
+    public Map<String, String> saveUser(HttpServletRequest request, String user_id, String group_id) {
+        setErrorMsg(request, "操作失败");
+        wechatUserService.updateGroup(user_id, group_id);
+        return retSuccess();
+    }
 
 }
--- src/com/fjx/wechat/mysdk/process/in/executor/InWechatEventSubscribeMsgExecutor.java
+++ src/com/fjx/wechat/mysdk/process/in/executor/InWechatEventSubscribeMsgExecutor.java
@@ -1,47 +1,48 @@
+
 package com.fjx.wechat.mysdk.process.in.executor;
 
-import java.util.Date;
-
+import com.fjx.wechat.base.admin.entity.WechatUserEntity;
+import com.fjx.wechat.base.admin.service.WechatUserService;
+import com.fjx.wechat.mysdk.beans.req.ReqEventMessage;
 import com.fjx.wechat.mysdk.constants.WechatReqEventConstants;
 import com.fjx.wechat.mysdk.constants.WechatReqMsgtypeConstants;
 import com.fjx.wechat.mysdk.context.WechatContext;
 import com.fjx.wechat.mysdk.tools.NameTool;
-import com.fjx.wechat.mysdk.beans.req.ReqEventMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.fjx.wechat.base.admin.entity.WechatUserEntity;
-import com.fjx.wechat.base.admin.service.WechatUserService;
-
+import java.util.Date;
 
 /**
  * 用户关注消息处理器
+ * 
  * @author fengjx xd-fjx@qq.com
  * @date 2014年9月11日
  */
 public class InWechatEventSubscribeMsgExecutor extends InServiceExecutor {
-	
-	@Autowired
-	private WechatUserService wechatUserService;
-	
-	@Override
-	public String execute() throws Exception {
-		ReqEventMessage eventMessage = new ReqEventMessage(WechatContext.getWechatPostMap());
-		logger.info("进入用户关注消息处理器fromUserName="+eventMessage.getFromUserName());
-		String event = eventMessage.getEvent();
-		
-		WechatUserEntity user = new WechatUserEntity();
-		user.setOpenid(eventMessage.getFromUserName());
-		user.setSubscribe_time(new Date());
-		user.setPublicAccountEntity(WechatContext.getPublicAccount());
-		wechatUserService.save(user);
-		
-		return doAction(null, WechatReqMsgtypeConstants.REQ_MSG_TYPE_EVENT, event, null);
-	}
 
-	@Override
-	public String getExecutorName() {
-		return NameTool.buildInServiceName(WechatReqMsgtypeConstants.REQ_MSG_TYPE_EVENT,
-				WechatReqEventConstants.EVENT_TYPE_SUBSCRIBE);
-	}
+    @Autowired
+    private WechatUserService wechatUserService;
+
+    @Override
+    public String execute() throws Exception {
+        ReqEventMessage eventMessage = new ReqEventMessage(WechatContext.getWechatPostMap());
+        logger.info("进入用户关注消息处理器fromUserName=" + eventMessage.getFromUserName());
+        String event = eventMessage.getEvent();
+
+        WechatUserEntity user = new WechatUserEntity();
+        user.setOpenid(eventMessage.getFromUserName());
+        user.setSubscribe_time(new Date());
+        user.setPublicAccountEntity(WechatContext.getPublicAccount());
+        wechatUserService.save(user);
+
+        return doAction(null, WechatReqMsgtypeConstants.REQ_MSG_TYPE_EVENT, event, null);
+    }
+
+    @Override
+    public String getExecutorName() {
+        return NameTool.buildInServiceName(WechatReqMsgtypeConstants.REQ_MSG_TYPE_EVENT,
+                WechatReqEventConstants.EVENT_TYPE_SUBSCRIBE);
+    }
 
 }
